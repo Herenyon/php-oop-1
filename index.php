afb6d8fb74cac5dbafd1ef5b6805a4dacab3afd6
Herenyon <rikitur17@gmail.com>
2023-04-14 17:10:26 +0200

    costruttore finito

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
         public $og_name;
         public $genre;
 
-        function __construct($_og_name)
+        function __construct($_og_name = null, $_genre= null)
         {
             $this->og_name = $_og_name;
-
+            $this->genre = $_genre;
         }
     }
 
@@ -14,8 +14,9 @@
     $interstellar->og_name = "Interstellar";
     $interstellar->genre = "Sci-Fi";
 
-    $dunkirk = new Movie ("Dunkirk");
+    $dunkirk = new Movie ("Dunkirk", "War");
     // echo $dunkirk->og_name;
+    // echo $dunkirk->genre;
 
     // $dunkirk->og_name = "Dunkirk";
     // $dunkirk->genre = "War";
