29e7a26b221c8cae2154ef5bde18ccf4ca4841a4
Herenyon <rikitur17@gmail.com>
2023-04-14 16:20:50 +0200

    prova costruttore

--- index.php
+++ index.php
@@ -2,15 +2,23 @@
     class Movie {
         public $og_name;
         public $genre;
+
+        function __construct($_og_name)
+        {
+            $this->og_name = $_og_name;
+
+        }
     }
 
     $interstellar = new Movie ();
     $interstellar->og_name = "Interstellar";
     $interstellar->genre = "Sci-Fi";
 
-    $dunkirk = new Movie ();
-    $dunkirk->og_name = "Dunkirk";
-    $dunkirk->genre = "War";
+    $dunkirk = new Movie ("Dunkirk");
+    // echo $dunkirk->og_name;
+
+    // $dunkirk->og_name = "Dunkirk";
+    // $dunkirk->genre = "War";
     var_dump($interstellar);
     var_dump($dunkirk);
 ?>
