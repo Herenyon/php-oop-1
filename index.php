640187c15ef1eb154ec252141e3dbe9a40ce21c7
Herenyon <rikitur17@gmail.com>
2023-04-14 16:02:06 +0200

    variabili

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <?php
     class Movie {
-
+        public $og_name;
+        public $genre;
     }
 
-    
+    $interstellar = new Movie ();
+    $interstellar->og_name = "Interstellar";
+    $interstellar->genre = "Sci-Fi";
+
+    $dunkirk = new Movie ();
+    $dunkirk->og_name = "Dunkirk";
+    $dunkirk->genre = "War";
+    var_dump($interstellar);
+    var_dump($dunkirk);
 ?>
